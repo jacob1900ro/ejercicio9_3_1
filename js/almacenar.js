7ec38879c59c42fbcaceeb790f657a812ae120bd
RocioPallas <rocio.pallasm@gmail.com>
2023-08-26 12:36:11 -0300

    Se optimiza el codigo para poder mostrar los datos cuardados al recargar la ventana

--- js/almacenar.js
+++ js/almacenar.js
@@ -1,30 +1,33 @@
 const btnAddItem = document.getElementById('agregar');
 const btnClear = document.getElementById('limpiar');
-
 const listContainer = document.getElementById('contenedor');
+const items = JSON.parse(localStorage.getItem('items')) || [];
+
+window.onload = () => {
+    addData();
+  };
+
+function addData() {
+    listContainer.innerHTML = '';
+    items.forEach((i) => {
+        listContainer.innerHTML += `<li class='liItems'>${i}</li>`;
+    });
+    document.getElementById('item').value = '';
+}
 
 btnAddItem.addEventListener('click', () => {
     const inputItem = document.getElementById('item').value;
     if (inputItem.trim() !== '') {
-        const items = JSON.parse(localStorage.getItem('items')) || [];
         items.push(inputItem);
         localStorage.setItem('items', JSON.stringify(items));
-        listContainer.innerHTML = '';
-        items.forEach((i) => {
-            listContainer.innerHTML += `<li class='liItems'>${i}</li>`;
-        });
-        document.getElementById('item').value = '';
+        addData();
     } else {
         alert('Los campos deben estar completos')
     }
-
-
 })
 
 btnClear.addEventListener('click', () => {
     localStorage.removeItem('items');
-    const clearList = document.querySelectorAll('.liItems');
-    clearList.forEach((x) => {
-        x.remove();
-    })
+    listContainer.innerHTML = '';
 });
+
